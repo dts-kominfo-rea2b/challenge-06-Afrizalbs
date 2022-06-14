5ebd77427085c9ef6f3554bbb5e4784c6f116510
Afrizal <afrizalbagas.03@gmail.com>
2022-06-14 15:46:48 +0700

    80%

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,63 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const parseData = (data) => {
+  return JSON.parse(data);
+};
+
+const splitData = (parsedData) => {
+  if (parsedData.message !== undefined) {
+    split = parsedData.message.split(" ");
+    const newData = split[1];
+    return newData;
+  }
+  if (parsedData[0].message !== undefined) {
+    split = parsedData[0].message.split(" ");
+    const newData = split[1];
+    return newData;
+  }
+  if (parsedData[0].data.message !== undefined) {
+    split = parsedData[0].data.message.split(" ");
+    const newData = split[1];
+    return newData;
+  }
+};
+
+const pushData = (data) => {
+  finalData.push(data);
+};
+
+let finalData = [];
+
+const bacaData = (callback) => {
+  fs.readFile(file1, "utf-8", (err, data) => {
+    if (err) {
+      return callback(err);
+    }
+    const dataHasilParse = parseData(data);
+    const newData = splitData(dataHasilParse);
+    pushData(newData);
+
+    fs.readFile(file2, "utf-8", (err, data) => {
+      if (err) {
+        return callback(err);
+      }
+      const dataHasilParse = parseData(data);
+      const newData = splitData(dataHasilParse);
+      pushData(newData);
+
+      fs.readFile(file3, "utf-8", (err, data) => {
+        if (err) {
+          return callback(err);
+        }
+        const dataHasilParse = parseData(data);
+        const newData = splitData(dataHasilParse);
+        pushData(newData);
+        callback(null, finalData);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
